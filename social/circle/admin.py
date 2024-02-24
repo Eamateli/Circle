13afd83692d38c69cf3c9a62ba400f69f3d50586
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-02-24 20:50:14 +0200

    Extend User Model unregister inital user and register user

--- social/circle/admin.py
+++ social/circle/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
-
+from django.contrib.auth.models import Group, User
+# Undergister Groups
 admin.site.unregister(Group)
+
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username"]
+    
+# Unregister initial User
+admin.site.unregister(User)
+# Reregister User
+admin.site.register(User, UserAdmin)
