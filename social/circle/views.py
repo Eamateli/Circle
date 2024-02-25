556e29ede566631f5f4630a829fbc269120979e2
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-02-25 20:28:31 +0200

    Adding the view function

--- social/circle/views.py
+++ social/circle/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+def profile_list(request):
+    return render(request, 'profile_list.html')
+    
