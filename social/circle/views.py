aef31ca0f3a3f6594bfe0f7dc182fb505207e892
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-02-25 20:41:14 +0200

    Showing profiles, excluding user

--- social/circle/views.py
+++ social/circle/views.py
@@ -1,8 +1,10 @@
+from profile import Profile
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
 
 def profile_list(request):
-    return render(request, 'profile_list.html')
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request, 'profile_list.html', {"profiles":profiles} )
     
