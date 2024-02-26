e89b52b8cfc4121ed989b74ead103e163233824d
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-02-26 14:32:17 +0200

    Adding the view for the profile

--- social/circle/views.py
+++ social/circle/views.py
@@ -16,3 +16,11 @@ def profile_list(request):
         return redirect('home')
         
     
+def profile(request,pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+        return render(request, "profile.html", {"profile":profile})
+    else:
+        messages.success(request, ("You must be logged in to View Page..."))
+        return redirect('home')
+        
