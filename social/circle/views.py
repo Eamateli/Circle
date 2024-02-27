0e68534465ce5c6deea75847d4fede2cbc79bb75
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-02-27 14:28:00 +0200

    Adding post form logic

--- social/circle/views.py
+++ social/circle/views.py
@@ -17,8 +17,21 @@ def profile_list(request):
         
     
 def profile(request,pk):
-    if request.user.is_authenticated:
+    if request.user.is_authenticated: 
         profile = Profile.objects.get(user_id=pk)
+        #Post form logic
+        if request.method == "POST":
+            #Get current user ID
+            current_user_profile = request.user.profile
+            # Get form data
+            action = request.POST['follow']
+            # Decide to follow or unfollow
+            if action == "unfollow":
+                current_user_profile.follows.remove(profile)
+            elif action == "follow":
+                current_user_profile.follows.add(profile)
+            # Save the profile
+            current_user_profile.save() 
         return render(request, "profile.html", {"profile":profile})
     else:
         messages.success(request, ("You must be logged in to View Page..."))
