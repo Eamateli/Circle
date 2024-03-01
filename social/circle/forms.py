f6375e8f93229363f32359031a4a7ade8390d8ba
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-03-01 16:36:32 +0200

    Adding a class Named SignUpForm and Meta

--- social/circle/forms.py
+++ social/circle/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Noise
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class NoiseForm(forms.ModelForm):
     body = forms.CharField(required=True,
@@ -15,3 +17,12 @@ class NoiseForm(forms.ModelForm):
     class Meta:
         model = Noise
         exclude = ("user",)
+        
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
+    first_name = forms.CharField(label="", max_length=100,widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
+    last_name = forms.CharField(label="", max_length=100,widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
+    
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1','password2')
