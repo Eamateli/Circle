78a31d6df9b08d7ba0636d5ad677d94b4f82e088
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-03-03 22:13:47 +0200

    Debugging

--- social/circle/views.py
+++ social/circle/views.py
@@ -98,22 +98,22 @@ def register_user(request):
     return render(request, "register.html", {'form':form})
     
 def update_user(request):
-    if request.user.is_authenticated:
-        current_user = User.objects.get(id=request.user.id)
-        profile_user = Profile.objects.get(user__id=request.user.id)
-        user_form = SignUpForm(request.POST or None, request.FILES or None, instance=current_user)
-        profile_form = ProfilePicForm(request.POST or None, request.FILES or None, instance=profile_user)
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-            login(request, current_user)
-            messages.success(request, ("Your profile has been Updated!"))  
-            return redirect('home')  
-        
-        
-        return render(request, "update_user.html", {'user_form':user_form, 'profile_form':profile_form})
-    
-    else:
-        messages.success(request, ("You Must be Logged In to view that page..."))  
-        return redirect('home')     
+	if request.user.is_authenticated:
+		current_user = User.objects.get(id=request.user.id)
+		profile_user = Profile.objects.get(user__id=request.user.id)
+		# Get Forms
+		user_form = SignUpForm(request.POST or None, request.FILES or None, instance=current_user)
+		profile_form = ProfilePicForm(request.POST or None, request.FILES or None, instance=profile_user)
+		if user_form.is_valid() and profile_form.is_valid():
+			user_form.save()
+			profile_form.save()
+
+			login(request, current_user)
+			messages.success(request, ("Your Profile Has Been Updated!"))
+			return redirect('home')
+
+		return render(request, "update_user.html", {'user_form':user_form, 'profile_form':profile_form})
+	else:
+		messages.success(request, ("You Must Be Logged In To View That Page..."))
+		return redirect('home')  
         
