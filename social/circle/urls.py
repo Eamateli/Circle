5fdf43899e3ab5bf38b6813296f90008829fed18
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-03-05 23:23:31 +0200

    Adding the url path

--- social/circle/urls.py
+++ social/circle/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
     path('update_user/', views.update_user, name='update_user'),
+    path('noise_like/<int:pk>', views.noise_like, name='noise_like'),
 
 ]
