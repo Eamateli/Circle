dea3aea1bb8f47637154c971b811cbe3c1e647ea
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-03-29 03:28:02 +0200

    Adding code to deploy

--- social/social/settings.py
+++ social/social/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')Run the command: py manage.py collectstatic to collect static files.
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
