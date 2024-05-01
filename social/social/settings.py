f93a9677d9e4b8a8fa8e6e0e25f26fef736b51e2
Ermal Aris Mateli <48600509+Eamateli@users.noreply.github.com>
2024-05-01 20:38:52 +0300

    Updates

--- social/social/settings.py
+++ social/social/settings.py
@@ -129,5 +129,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True 
+# CSRF_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True 
